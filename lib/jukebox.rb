8c57db542623a99611c61bb56ad6bd781f29a2b6
George Kirby <georgeskirby@btinternet.com>
2019-07-31 08:59:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -34,7 +34,7 @@ def play(songs)
   if songs.find{|song| song == user_input}
     puts "Playing #{song}"
   elsif songs.length <= user_input.to_i
-      puts "Playing #{songs[user_input+1]}"
+      puts "Playing #{songs[user_input.to_i+1]}"
   else
     puts "Invalid input, please try again"
   end
