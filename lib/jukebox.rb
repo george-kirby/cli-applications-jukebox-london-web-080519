157bbb3a518b47a9a24e30f3e135ae570292d3c0
George Kirby <georgeskirby@btinternet.com>
2019-07-31 08:33:16 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,2 +1,37 @@
 # Add your code here
 
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  
+end
+
+
+def play
+  
+end
+
+
+def list
+  
+end
+
+
+def exit
+  
+end
+
+
+def run
+  
+end
